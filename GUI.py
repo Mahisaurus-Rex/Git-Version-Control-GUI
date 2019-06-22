da6d23ea4744c9c562ebbd3afbf7b028656359ff
Mahi <mahipasarkar@gmail.com>
2019-06-22 18:18:21 -0400

    ligma

--- GUI.py
+++ GUI.py
@@ -4,12 +4,11 @@ from tkinter import filedialog
 
 class GUI:
     def __init__(self, master):
-        resize_img()
         self.master = master
         master.title("Easy Git")
 
         
 root = Tk()
-root.geometry("925x510") #GUI start size
+root.geometry("640x480") #GUI start size
 my_gui = GUI(root)
 root.mainloop()
